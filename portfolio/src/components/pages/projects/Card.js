7a4a2a764b3c3ad78abd0a28c54699eb25fcfdc9
ClayData <claytonortiz96@gmail.com>
2020-05-04 14:25:26 -0500

    cards build out with modals

--- portfolio/src/components/pages/projects/Card.js
+++ portfolio/src/components/pages/projects/Card.js
@@ -6,24 +6,25 @@ function Card(props){
             <div className="img-container">
                 <img alt={props.name} src={props.image} />
             </div>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                <p>{props.name}</p>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                 Launch demo modal
             </button> 
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
+                    <div className="modal-body">
                         ...
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" href={props.site}>Site</button>
+                        <button type="button" className="btn btn-primary" href={props.github}>Git Repo</button>
                     </div>
                     </div>
                 </div>
--- portfolio/src/components/pages/projects/Projects.js
+++ portfolio/src/components/pages/projects/Projects.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Card from './Card';
+import data from './projectData.json';
 
-function Projects() {
-    return(
-    <div>
-        Projects
-    </div>
-    )
+class Projects extends React.Component {
+    constructor(props){
+    super(props);
+    this.state ={
+        info: data
+    }
+}
+
+    render() {
+        return(
+            <div className="container">
+                {this.state.info.map(project => (
+                    <Card 
+                    key={project.id}
+                    image={project.image}
+                    name={project.name}
+                    site={project.site}
+                    gitube={project.github}
+                    />
+                ))}
+                
+            </div>
+        )
+    }
 }
 
 export default Projects;
