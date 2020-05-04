67070a291315d6c06a71562ce574453f756165fc
ClayData <claytonortiz96@gmail.com>
2020-05-04 13:47:04 -0500

    set up routing

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function NavBar() {
-
+    const location = useLocation();
+    return(
+    <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/about" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    About
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/projects" className={location.pathname === "/projects" ? "nav-link active" : "nav-link"}>
+                    Projects
+                </Link>
+            </li>
+        </ul>
+    )
 }
 
 export default NavBar;
