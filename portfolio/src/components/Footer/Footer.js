72a9873e6736a970dc0c59109577c93147e764da
ClayData <claytonortiz96@gmail.com>
2020-05-06 23:02:03 -0500

    centered bottom icons

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, BottomNavigation, BottomNavigationAction, Paper } from '@material-ui/core';
+import { makeStyles, BottomNavigation, BottomNavigationAction, Paper, Grid } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
@@ -7,6 +7,7 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 const useStyles = makeStyles({
   root: {
     width: 500,
+    margin: 'auto'
   },
 });
 
@@ -15,19 +16,25 @@ export default function Footer() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Paper>
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        showLabels
-        className={classes.root}
-      >
-        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/in/clayton-ortiz-844340108/" />
-        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} href="https://github.com/ClayData"/>
-        <BottomNavigationAction label="Email" icon={<MailOutlineIcon />} href="mailto:claytonortiz96@gmail.com"/>
-      </BottomNavigation>
-    </Paper>
+    <Grid container>
+      
+        <Grid item xs={12}>
+          <Paper elevation={0}>
+            <BottomNavigation
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              showLabels
+              className={classes.root}
+            >
+              <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/in/clayton-ortiz-844340108/" />
+              <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} href="https://github.com/ClayData"/>
+              <BottomNavigationAction label="Email" icon={<MailOutlineIcon />} href="mailto:claytonortiz96@gmail.com"/>
+            </BottomNavigation>
+          </Paper>
+        </Grid>
+      
+    </Grid>
   );
 }
